e92bc9530cac5c1d8beb5f64d4ae310c5953b339
sahana <hana@girlin.tech>
2026-02-01 23:24:01 -0500

    OKAY THIS IS GOOD

--- src/help.ts
+++ src/help.ts
@@ -1,8 +1,8 @@
 export default `
 Usage:
-  cds <pin>[/path]							- Change directory to the one associated with <pin>
-  cds list 											- List all pins and their associated directories
-  cds pin <tag> [directory]   	- Pin [directory] (default: current working directory) to <tag>
-  cds unpin <tag>								- Remove the pin association
-  cds                   				- Show this help
+  cds <pin>                    - Change directory to the one associated with <pin>
+  cds list                     - List all pins and their associated directories
+  cds pin <tag> [directory]    - Pin [directory] (default: current working directory) to <tag>
+  cds unpin <tag>              - Remove the pin association
+  cds                          - Show this help
 `.trim()
