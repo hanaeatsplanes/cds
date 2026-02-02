6aa659810c09c9d781b86ddd4138a631f25c5123
sahana <hana@girlin.tech>
2026-02-01 23:19:30 -0500

    asdmakd

--- src/help.ts
+++ src/help.ts
@@ -1,6 +1,6 @@
 export default `
 Usage:
-	cds <pin>[/path]							- Change directory to the one associated with <pin>
+  cds <pin>[/path]							- Change directory to the one associated with <pin>
   cds list 											- List all pins and their associated directories
   cds pin <tag> [directory]   	- Pin [directory] (default: current working directory) to <tag>
   cds unpin <tag>								- Remove the pin association
