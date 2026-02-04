26a4bb982fcbfaf66c36611dc53eab5766ecce1c
sahana <hana@girlin.tech>
2026-02-04 10:46:36 -0500

    askdjakaaad

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,9 @@ const args = process.argv.slice(2);
 const arg0 = args[0]
 if (!arg0 || arg0 === "help") {
 	console.log(`echo "${help}"`)
-} else {
-
+	process.exit(arg0 ? 1 : 0);
 }
+
+export const pwd = process.cwd();
+
+console.log(`echo "you are at ${pwd}, args: ${JSON.stringify(args)}`);
