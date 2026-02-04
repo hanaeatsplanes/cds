19e315862191904237e87b5cc8f5958ecc40e025
sahana <hana@girlin.tech>
2026-02-03 20:53:26 -0500

    sourceajskdjak

--- sourceme.sh
+++ sourceme.sh
@@ -1,7 +1,10 @@
-#!/bin/bash
+(return 0 2>/dev/null) || {
+  echo "source this file, don't run it!"
+  exit 1
+}
 
 cds() {
-  eval "$(bun run . "$@")"
+  eval "$(bun run ~/Documents/projects/cds "$@")"
 }
 
 export -f cds
