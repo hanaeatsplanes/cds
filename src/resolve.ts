0cce2b369118802459b6e0670967003f298a7572
sahana <hana@girlin.tech>
2026-02-04 23:00:31 -0500

    biome

--- src/resolve.ts
+++ src/resolve.ts
@@ -2,7 +2,7 @@ import { realpathSync } from "node:fs";
 import { resolve } from "node:path";
 
 // TODO: add tag resolve
-export default function(input: string) {
+export default function (input: string) {
 	if (input.startsWith("~")) {
 		// biome-ignore lint/complexity/useLiteralKeys: TS4111
 		input = input.replace(/^~/, process.env["HOME"] || "");
