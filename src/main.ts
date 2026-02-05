ef03201fe3dc2c159c7fec1cb6af3226e3aff9ba
sahana <hana@girlin.tech>
2026-02-04 22:43:26 -0500

    mnmnm

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,6 @@ const args = process.argv.slice(2);
 const arg0 = args[0];
 if (arg0 === "help" || arg0 === "--help" || arg0 === "-h") {
 	console.log(`E"${help}"`);
-	process.exit(arg0 ? 1 : 0);
 }
 
 console.log(`P${resolve(arg0 || "~")}`);
